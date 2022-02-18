fe4082dd26af3a681fbb6ac93666daddf23a6135
Skydumb <83810424+Skydumb@users.noreply.github.com>
2022-02-18 21:25:15 +0100

    Add project files.

---
+++ opentk_example/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+
+namespace opentk_example
+{
+    class Program
+    {
+        static void Main()
+        {
+            using Window window = new Window(800, 600, "Window");
+            window.Run();
+        }
+    }
+    class Window : GameWindow
+    {
+        private readonly float[] _vertices =
+{
+            -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+             0.5f, -0.5f, 0.0f, // Bottom-right vertex
+             0.0f,  0.5f, 0.0f  // Top vertex
+        };
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        private Shader _shader;
+
+        public Window(int height, int width, string name) :
+            base(new GameWindowSettings(), new NativeWindowSettings()
+            {
+                Size = new Vector2i(width, height),
+                Title = name
+            })
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape)) this.Close();
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(1f, 1f, 1f, 1f);
+            _vertexBufferObject = GL.GenBuffer();
+            // Binding VBO
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            // Creation of VAO
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            // Adding Shader
+            _shader = new Shader("Shaders/Shader.vert", "Shaders/Shader.frag");
+            _shader.Use();
+
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+        }
+    }
+}
---
+++ opentk_example/Shader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using OpenTK.Graphics.OpenGL4;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opentk_example
+{
+    // Class for adding basic shader
+    public class Shader
+    {
+        private readonly int Handle;
+        private readonly Dictionary<string, int> _uniformLocations;
+
+        public Shader(string vertPath, string fragPath)
+        {
+            var shaderSource = File.ReadAllText(vertPath);
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, shaderSource);
+            CompileShader(vertexShader);
+
+            shaderSource = File.ReadAllText(fragPath);
+            var fragShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragShader, shaderSource);
+            CompileShader(fragShader);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragShader);
+            LinkProgram(Handle);
+
+            GL.DetachShader(Handle, fragShader);
+            GL.DetachShader(Handle, vertexShader);
+            GL.DeleteShader(fragShader);
+            GL.DeleteShader(vertexShader);
+
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out int numberOfUniforms);
+            _uniformLocations = new Dictionary<string, int>();
+            for (int i = 0; i < numberOfUniforms; i++)
+            {
+                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+                var value = GL.GetUniformLocation(Handle, key);
+                _uniformLocations.Add(key, value);
+            }
+        }
+        // Making debugging easier
+        private static void LinkProgram(int handle)
+        {
+            GL.LinkProgram(handle);
+            GL.GetProgram(handle, GetProgramParameterName.LinkStatus, out int code);
+            if (code != (int)All.True)
+            {
+                var infolog = GL.GetProgramInfoLog(handle);
+                throw new Exception($"Error occured when linking Program({handle}).\n\n{infolog}");
+            }
+        }
+
+        private static void CompileShader(int shader)
+        {
+            GL.CompileShader(shader);
+            // Check for compilation errors
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out var code);
+            if (code != (int)All.True)
+            {
+                var infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Error occurred whilst compiling Shader({shader}).\n\n{infoLog}");
+            }
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+        public void SetInt(string name, int data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+    }
+}
